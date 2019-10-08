2ab52a4456fa287cb0d1d0c52abde916666d57b0
Douglas AC <dac17@inf.ufpr.br>
2019-10-08 17:57:16 -0300

    limpando a casa

--- LAB_1/OrCS/OrCS-Bi-Mode-2048/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./orcs -t ../tests/astar.CINT.PP200M > saida/saida_astar
-./orcs -t ../tests/calculix.CFP.PP200M > saida/saida_calculix
-./orcs -t ../tests/dealII.CFP.PP200M > saida/saida_dealII
-./orcs -t ../tests/gromacs.CFP.PP200M > saida/saida_gromacs
-./orcs -t ../tests/libquantum.CINT.PP200M > saida/saida_libquantum
-./orcs -t ../tests/namd.CFP.PP200M > saida/saida_namd
--- LAB_1/OrCS/OrCS-Bi-Mode/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./orcs -t disposable/astar/astar.CINT.PP200M > saida/saida_astar
-./orcs -t disposable/calculix/calculix.CFP.PP200M > saida/saida_calculix
-./orcs -t disposable/dealII/dealII.CFP.PP200M > saida/saida_dealII
-./orcs -t disposable/gromacs/gromacs.CFP.PP200M > saida/saida_gromacs
-./orcs -t disposable/libquantum/libquantum.CINT.PP200M > saida/saida_libquantum
-./orcs -t disposable/namd/namd.CFP.PP200M > saida/saida_namd
