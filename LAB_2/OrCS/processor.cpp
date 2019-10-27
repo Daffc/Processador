a9312d30d35880a48807b7bcd9671bf023935262
Douglas A.C <dac17@inf.ufpr.br>
2019-10-27 16:07:51 -0300

    Lógica de read completa (com adição de delay)

--- LAB_2/OrCS/processor.cpp
+++ LAB_2/OrCS/processor.cpp
@@ -14,105 +14,26 @@ processor_t::processor_t() {
 	/*--------------------- TESTES LEITURA ---------------------*/
 	/*----------------------------------------------------------*/
 	
-	uint32_t posicao_1, posicao_2, endereco;
-	endereco = 2579;
-	// delay = L1->latencia.
+	// uint32_t posicao_1, posicao_2, endereco;
+	// endereco = 2579;
+	// // delay = L1->latencia.
 
-	// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
-	L1->imprimeGrupo(endereco);
-	L2->imprimeGrupo(endereco);
+	// // Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
+	// L1->imprimeGrupo(endereco);
+	// L2->imprimeGrupo(endereco);
 
-	if(!L1->search(endereco, &posicao_1)){
-		// delay += L2->latencia.
+	// if(!L1->search(endereco, &posicao_1)){
+	// 	// delay += L2->latencia.
 
-		// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
-		if(!L2->search(endereco, &posicao_2)){
-			// delay += DELAY_PRINC_MEM.
+	// 	// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
+	// 	if(!L2->search(endereco, &posicao_2)){
+	// 		// delay += DELAY_PRINC_MEM.
 
-			L2->allocate(endereco, posicao_2);
-		}
+	// 		L2->allocate(endereco, posicao_2);
+	// 	}
 
-		L1->allocate(endereco, posicao_1);
-	}
-	
-	L1->imprimeGrupo(endereco);
-	L2->imprimeGrupo(endereco);
-
-	endereco = 2583;
-
-	if(!L1->search(endereco, &posicao_1)){
-		// delay += L2->latencia.
-
-		// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
-		if(!L2->search(endereco, &posicao_2)){
-			// delay += DELAY_PRINC_MEM.
-
-			L2->allocate(endereco, posicao_2);
-		}
-
-		L1->allocate(endereco, posicao_1);
-	}
-	
-	L1->imprimeGrupo(endereco);
-	L2->imprimeGrupo(endereco);
-
-	endereco = 2591;
-	if(!L1->search(endereco, &posicao_1)){
-		// delay += L2->latencia.
-
-		// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
-		if(!L2->search(endereco, &posicao_2)){
-			// delay += DELAY_PRINC_MEM.
-
-			L2->allocate(endereco, posicao_2);
-		}
-
-		L1->allocate(endereco, posicao_1);
-	}
-	
-	L1->imprimeGrupo(endereco);
-	L2->imprimeGrupo(endereco);
-
-	ORCS_PRINTF("----------------------------\n");
-
-	endereco = 2607;
-	if(!L1->search(endereco, &posicao_1)){
-		// delay += L2->latencia.
-
-		// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
-		if(!L2->search(endereco, &posicao_2)){
-			// delay += DELAY_PRINC_MEM.
-
-			L2->allocate(endereco, posicao_2);
-		}
-
-		L1->allocate(endereco, posicao_1);
-	}
-	
-	L1->imprimeGrupo(endereco);
-	L2->imprimeGrupo(endereco);
-
-	ORCS_PRINTF("----------------------------\n");
-	
-	endereco = 2603;
-	if(!L1->search(endereco, &posicao_1)){
-		// delay += L2->latencia.
-
-		// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
-		if(!L2->search(endereco, &posicao_2)){
-			// delay += DELAY_PRINC_MEM.
-
-			L2->allocate(endereco, posicao_2);
-		}
-
-		L1->allocate(endereco, posicao_1);
-	}
-	
-	L1->imprimeGrupo(endereco);
-	L2->imprimeGrupo(endereco);
-	ORCS_PRINTF("----------------------------\n");
-
-	
+	// 	L1->allocate(endereco, posicao_1);
+	// }
 
 	/*----------------------------------------------------------*/
 	/*------------------- FIM TESTES LEITURA -------------------*/
@@ -129,20 +50,71 @@ void processor_t::clock() {
 
 	/// Get the next instruction from the trace
 	opcode_package_t new_instruction;
-	// uint64_t  group, older;
-	// char selected;
-	
-	if(delay){
-		delay--;
-		return;	
-	}
-	if (!orcs_engine.trace_reader->trace_fetch(&new_instruction)) {
+	uint32_t posicao_1, posicao_2, endereco;
+
+	// if(delay){
+	// 	delay--;
+	// 	return;	
+	// }
+	if (!orcs_engine.trace_reader->trace_fetch(&new_instruction) || orcs_engine.global_cycle > 200) {
 		/// If EOF
 		ORCS_PRINTF("CLOCKS = %" PRIu64 "\n",orcs_engine.global_cycle);
 		orcs_engine.simulator_alive = false;
 	}
 	else{
+		if(new_instruction.is_read){
 
+			endereco = new_instruction.read_address;
+
+			ORCS_PRINTF("Buscando:\t%" PRIu32 "\n\n", endereco);
+			L1->imprimeGrupo(endereco);
+			L2->imprimeGrupo(endereco);
+
+			delay = L1->latencia;
+
+			if(!L1->search(endereco, &posicao_1)){
+				delay += L2->latencia;
+
+				// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
+				if(!L2->search(endereco, &posicao_2)){
+					delay += DELAY_PRINC_MEM;
+
+					L2->allocate(endereco, posicao_2);
+					
+				}
+
+				L1->allocate(endereco, posicao_1);
+			}
+			ORCS_PRINTF("DELAY:\t%d\n\n", delay);
+			ORCS_PRINTF("--------------------------------------\n\n");
+		}
+		if(new_instruction.is_read2){
+
+			
+			endereco = new_instruction.read2_address;
+			
+			ORCS_PRINTF("Buscando:\t%" PRIu32 "\n\n", endereco);
+			L1->imprimeGrupo(endereco);
+			L2->imprimeGrupo(endereco);
+
+			delay = L1->latencia;
+
+			// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
+
+			if(!L1->search(endereco, &posicao_1)){
+				delay += L2->latencia;
+
+				// Verifica se bloco está em cache e o atualiza, caso não esteja entra no if.
+				if(!L2->search(endereco, &posicao_2)){
+					delay += DELAY_PRINC_MEM;
+
+					L2->allocate(endereco, posicao_2);
+				}
+				L1->allocate(endereco, posicao_1);
+			}
+			ORCS_PRINTF("DELAY:\t%d\n\n", delay);
+			ORCS_PRINTF("--------------------------------------\n\n");
+		}
 	}
 };
 
@@ -174,16 +146,18 @@ int cache::search(uint32_t endereco, uint32_t* melhor_posicao){
 	grupo = index * this->quantidade_vias;
 
 	older = UINT32_MAX;
+
+	// Deifine primeiro elemento do conjunto associativo como "selected".
 	selected = grupo;
 	
-	ORCS_PRINTF("%s->search:\n", this->nome);
-	ORCS_PRINTF("endereco:\t%" PRIu32 "\n", endereco);
+	// ORCS_PRINTF("%s->search:\n", this->nome);
+	// ORCS_PRINTF("endereco:\t%" PRIu32 "\n", endereco);
 
 	for( i = 0; i < this->quantidade_vias; i++){
 		// Caso bloco procurado seja encontrado e esteja válido, atualize o seu tempo de acesso e retorne 1.
 		if((this->blocos[grupo + i].endereco == endereco) && this->blocos[grupo + i].validade ){
-			ORCS_PRINTF("ENCONTRADO!\n");
-			ORCS_PRINTF("posicao:\t%" PRIu32 "\n\n\n", grupo + i);
+			// ORCS_PRINTF("ENCONTRADO!\n");
+			// ORCS_PRINTF("posicao:\t%" PRIu32 "\n\n\n", grupo + i);
 			this->blocos[grupo + i].time = orcs_engine.global_cycle;
 			return 1;
 		}
@@ -192,14 +166,15 @@ int cache::search(uint32_t endereco, uint32_t* melhor_posicao){
 		if(this->blocos[selected].validade){
 			// Caso bloco "selected" seja válido, verifica se próximo é mais 
 			// antigo que o já selecionado ou se é um bloco inválido.
-			if(older > this->blocos[grupo + i].time || !this->blocos[grupo + i].validade){
+			if((older > this->blocos[grupo + i].time) || !(this->blocos[grupo + i].validade)){
 				selected = grupo + i;
+				older = this->blocos[grupo + i].time;
 			}
 		}
 	}
 
-	ORCS_PRINTF("NÂO ENCONTRADO!\n\n");
-	ORCS_PRINTF("posicao:\t%" PRIu32 "\n\n\n", selected);
+	// ORCS_PRINTF("NÂO ENCONTRADO!\n\n");
+	// ORCS_PRINTF("posicao:\t%" PRIu32 "\n\n\n", selected);
 
 	*melhor_posicao = selected;
 
@@ -247,8 +222,8 @@ void cache::imprimeGrupo(uint32_t endereco){
 		ORCS_PRINTF("endereco: %" PRIu32 " \t", this->blocos[grupo + i].endereco);	
 		ORCS_PRINTF("time: %" PRIu64 " \t", this->blocos[grupo + i].time);
 		ORCS_PRINTF("validade: %d \t", this->blocos[grupo + i].validade);
-		ORCS_PRINTF("dirty: %d \t", this->blocos[grupo + i].dirty);
-		ORCS_PRINTF("\n\n");
+		ORCS_PRINTF("dirty: %d \n", this->blocos[grupo + i].dirty);
 	}
+	ORCS_PRINTF("\n");
 
 }
